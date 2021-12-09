f5dc90b739c393503b54d8b9c9da2b2cebe73ca8
Bader <Baderasiri16@gmail.com>
2021-12-09 07:15:56 +0300

    More fixes

--- get_live_info.py
+++ get_live_info.py
@@ -1,7 +1,5 @@
 import requests
 import json
-from typing import Dict, Any
-import hashlib
 import time
 
 #codes to know what did the player did
@@ -18,53 +16,75 @@ codes = {'36': 1, '37': 1, '39': 1, '63': 1,
 def store(players):
     teams = {}
     y = 0
-    for k, kv in players.items():
-        y += 1
-        teams[f"team{y}"] = [{x: {'OG': 0, 'Goals': 0, 'Assist': 0, 'Cards': ''}}
-                             for x in kv]
+    if len(teams) != 0:
+        for k, kv in players.items():
+
+            teams[team_names[y]] = [{x: {'OG': 0, 'Goals': 0, 'Assist': 0, 'Cards': ''}}
+                                    for x in kv]
+            y += 1
+    else:
+        for i in team_names:
+            teams[i] = []
     with open('results.json', 'w') as file:
         file.write(json.dumps(teams))
-    return teams
 
 #get the players from the GET request returns json file
 
 
 def get_players(Pjson):
-    global Players_shortcuts
+    global Players_shortcuts, team_names
+    team_names = []
     pn = ''
     Players_shortcuts = {}
     teams = {}
     lst = []
     info = json.loads(Pjson.text)
-    Lu = info['Lu']
-    y = 0
-    while y < 2:
-        Ps = Lu[y]['Ps']
-        for k in Ps:
-            try:
-                pn = k['Snm']
-                lst.append(pn)
-                Players_shortcuts[k['Snm'][0].capitalize(
-                ) + '. ' + pn.split(' ')[1]] = pn
-            except:
+    team_names.append(info['T1'][0]['Nm'])
+    team_names.append(info['T2'][0]['Nm'])
+    try:
+        Lu = info['Lu']
+        y = 0
+        while y < 2:
+            Ps = Lu[y]['Ps']
+            for k in Ps:
                 try:
-                    pn += k['Fn'] + ' ' + k['Ln']
+                    pn = k['Snm']
                     lst.append(pn)
-                    Players_shortcuts[k['Fn']
-                                      [0].capitalize() + '. ' + k['Ln']] = pn
+                    Players_shortcuts[k['Snm'][0].capitalize(
+                    ) + '. ' + pn.split(' ')[1]] = pn
                 except:
-                    pn = k['Ln']
-                    Players_shortcuts[k['Ln']] = pn
-                    lst.append(pn)
+                    try:
+                        pn += k['Fn'] + ' ' + k['Ln']
+                        lst.append(pn)
+                        Players_shortcuts[k['Fn']
+                                          [0].capitalize() + '. ' + k['Ln']] = pn
+                    except:
+                        pn = k['Ln']
+                        Players_shortcuts[k['Ln']] = pn
+                        lst.append(pn)
 
-            pn = ''
-        teams[f'team{y+1}'] = lst.copy()
-        lst.clear()
-        y += 1
-
+                pn = ''
+            teams[f'team{y+1}'] = lst.copy()
+            lst.clear()
+            y += 1
+    except:
+        pass
     return teams
 
 #request
+
+
+def missing_players(Player_name, code, eventid):
+
+    with open('results.json', 'r') as f:
+        data = json.loads(f.read())
+
+    with open('results.json', 'w') as f:
+        ss = list(data)
+        data[ss[eventid - 1]].append({Player_name: {'OG': 0, 'Goals': 0, 'Assist': 0, 'Cards': ''}})
+        f.write(json.dumps(data))
+    apply_stat(Player_name, code, eventid)
+    pass
 
 
 def request(url):
@@ -86,6 +106,7 @@ def get_stat(url):
     for k, v in Incs:
         for x in v:
             try:
+                eventid = x['Incs'][0]['Nm']
                 ha = hashing(x['Incs'][0])
                 ha2 = hashing(x['Incs'][1])
                 if ha not in Hashes and ha2 not in Hashes:
@@ -97,33 +118,41 @@ def get_stat(url):
                         Second_player_name = x['Incs'][1]['Pn']
                     Player_code = str(x['Incs'][0]['IT'])
                     Second_player_code = str(x['Incs'][1]['IT'])
-                    apply_stat(Player_name, Player_code)
-                    apply_stat(Second_player_name, Second_player_code)
+                    apply_stat(Player_name, Player_code, eventid)
+                    apply_stat(Second_player_name, Second_player_code, eventid)
                     Hashes.append(ha)
                     Hashes.append(ha2)
                 else:
                     pass
 
             except:
-                ha = hashing(x)
-                if ha not in Hashes:
-                    try:
-                        Player_name = Players_shortcuts[x['Pn']]
-                    except:
-                        Player_name = x['Pn']
+                try:
+                    eventid = x['Nm']
+                    ha = hashing(x)
+                    if ha not in Hashes:
+                        try:
+                            Player_name = Players_shortcuts[x['Pn']]
+                        except:
+                            Player_name = x['Pn']
+                            Player_code = str(x['IT'])
+                            apply_stat(Player_name, Player_code, eventid)
+                            Hashes.append(ha)
+                    else:
+                        pass
+                except:
+                    Player_name = 'Unknown'
                     Player_code = str(x['IT'])
-                    apply_stat(Player_name, Player_code)
+                    apply_stat(Player_name, Player_code, eventid)
                     Hashes.append(ha)
-                else:
-                    pass
 
 
-def apply_stat(Pname, code):
+def apply_stat(Pname, code, eventid):
     with open('results.json', 'r') as f:
         data = json.loads(f.read())
         for k, v in data.items():
             for i in v:
                 for s, ss in i.items():
+
                     if s == Pname:
                         if code == '44':
                             ss[codess[code]] = ""
@@ -131,18 +160,19 @@ def apply_stat(Pname, code):
                             pass
                         ss[codess[code]] += codes[code]
                         print(ss, Pname)
+                        with open('results.json', 'w') as f:
+                            f.write(json.dumps(data))
+                        return
                     else:
                         pass
-    with open('results.json', 'w') as f:
-        f.write(json.dumps(data))
+    missing_players(Pname, code, eventid)
 
 
-url = 'https://prod-public-api.livescore.com/v1/api/react/match-x/soccer/534916/3.00?MD=1'
-try:
-    store(get_players(request(url)))
-except Exception as e:
-    print('store: ', e)
-    pass
+
+
+url = 'https://prod-public-api.livescore.com/v1/api/react/match-x/soccer/590422/3.00?MD=1'
+
+store(get_players(request(url)))
 
 
 def main():
@@ -152,16 +182,11 @@ def main():
     data = json.loads(r.text)
     while data['Eps'] != 'FT':
         data = json.loads(request(url).text)
-        try:
-            print(data['Eps'])
-            get_stat(url)
-        except Exception as e:
-            print("get: ", e)
-            pass
+
+        print(data['Eps'])
+        get_stat(url)
+
         time.sleep(60)
 
 
-try:
-    main()
-except KeyboardInterrupt:
-    print('Done')
+main()
